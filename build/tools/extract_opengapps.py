c03052ed9983428f130c2180cef8b2e39925502a
Philippe Bellanger <pbellanger@hotmail.com>
2016-04-21 17:24:34 -0400

    Updated extract_opengapps.py script for compatibility with 04/21/2016 version of opengapps
    Note: gDocs, gSheets, gSlides, gPlus, and PlayGames are only compatible with 480 dpi screens
          and therefore script no longer works with these apps for nakasig (213 dpi) or occam (320 dpi)

--- build/tools/extract_opengapps.py
+++ build/tools/extract_opengapps.py
@@ -2,125 +2,122 @@
 import os, sys
 import shutil
 
-# List of opengapps apks ('super' build variant) following the format: (Location, opengapps_appname, dpis, priv, appname)
+# List of opengapps apks ('super' build variant) following the format: (Location, opengapps_appname, build_arch, dpis, priv, appname)
 # Currently excluded from the lists below: gFi, speech, indic, pinyin, zhuyin, japanese, korean, street
 COMMON_APKS = (
-	('Core', 'configupdater', ('nodpi', ), 'priv-app', 'ConfigUpdater'),
-	('Core', 'gmscore', ('nodpi', '240', '320', '480'), 'priv-app', 'PrebuiltGmsCore'),
-	('Core', 'googlebackuptransport', ('nodpi', ), 'priv-app', 'GoogleBackupTransport'),
-	('Core', 'googlecontactssync', ('nodpi', ), 'app', 'GoogleContactsSyncAdapter'),
-	('Core', 'googlefeedback', ('nodpi', ), 'priv-app', 'GoogleFeedback'),
-	('Core', 'googleonetimeinitializer', ('nodpi', ), 'priv-app', 'GoogleOneTimeInitializer'),
-	('Core', 'googlepartnersetup', ('nodpi', ), 'priv-app', 'GooglePartnerSetup'),
-	('Core', 'gsfcore', ('nodpi', ), 'priv-app', 'GoogleServicesFramework'),
-	('Core', 'gsflogin', ('nodpi', ), 'priv-app', 'GoogleLoginService'),
-	('Core', 'vending', ('nodpi', ), 'priv-app', 'Phonesky'),
+	('Core', 'configupdater', 'all', ('nodpi', ), 'priv-app', 'ConfigUpdater'),
+	('Core', 'gmscore', 'arm', ('nodpi', '240', '320', '480'), 'priv-app', 'PrebuiltGmsCore'),
+	('Core', 'googlebackuptransport', 'all', ('nodpi', ), 'priv-app', 'GoogleBackupTransport'),
+	('Core', 'googlecontactssync', 'all', ('nodpi', ), 'app', 'GoogleContactsSyncAdapter'),
+	('Core', 'googlefeedback', 'all', ('nodpi', ), 'priv-app', 'GoogleFeedback'),
+	('Core', 'googleonetimeinitializer', 'all', ('nodpi', ), 'priv-app', 'GoogleOneTimeInitializer'),
+	('Core', 'googlepartnersetup', 'all', ('nodpi', ), 'priv-app', 'GooglePartnerSetup'),
+	('Core', 'gsfcore', 'all', ('nodpi', ), 'priv-app', 'GoogleServicesFramework'),
+	('Core', 'gsflogin', 'all', ('nodpi', ), 'priv-app', 'GoogleLoginService'),
+	('Core', 'vending', 'all', ('nodpi', ), 'priv-app', 'Phonesky'),
 #	('GApps', 'androidforwork', ('nodpi',), 'priv-app', 'AndroidForWork'),
-	('GApps', 'androidpay', ('nodpi', '480', '640',), 'app', 'Wallet'),
-	('GApps', 'books', ('nodpi', ), 'app', 'Books'),
-	('GApps', 'calculatorgoogle', ('nodpi', ), 'app', 'CalculatorGoogle'),
-	('GApps', 'calendargoogle', ('nodpi', ), 'app', 'CalendarGooglePrebuilt'),
-	('GApps', 'calsync', ('nodpi', ), 'app', 'GoogleCalendarSyncAdapter'),
-	('GApps', 'calsync', ('nodpi', ), 'app', 'GoogleCalendarSyncAdapter'),
-	('GApps', 'chrome', ('nodpi', ), 'app', 'Chrome'),
-	('GApps', 'clockgoogle', ('nodpi', ), 'app', 'PrebuiltDeskClockGoogle'),
-	('GApps', 'cloudprint', ('nodpi', ), 'app', 'CloudPrint2'),
-	('GApps', 'contactsgoogle', ('nodpi', ), 'priv-app', 'GoogleContacts'),
-	('GApps', 'dmagent', ('nodpi', ), 'app', 'DMAgent'),
-	('GApps', 'docs', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsDocs'),
-	('GApps', 'drive', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'Drive'),
-	('GApps', 'ears', ('nodpi', ), 'app', 'GoogleEars'),
-	('GApps', 'earth', ('nodpi', '120-160', '213-240', '320', '480',), 'app', 'GoogleEarth'),
-	('GApps', 'exchangegoogle', ('nodpi', ), 'app', 'PrebuiltExchange3Google'),
-	('GApps', 'faceunlock', ('nodpi', ), 'app', 'FaceLock'),
-	('GApps', 'fitness', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'FitnessPrebuilt'),
-	('GApps', 'gcs', ('nodpi', ), 'priv-app', 'GCS'),
-	('GApps', 'gmail', ('nodpi', ), 'app', 'PrebuiltGmail'),
-	('GApps', 'googlenow', ('nodpi', ), 'app', 'GoogleHome'),
-	('GApps', 'googleplus', ('nodpi', '480',), 'app', 'PlusOne'),
-	('GApps', 'googletts', ('nodpi', ), 'app', 'GoogleTTS'),
-	('GApps', 'hangouts', ('160', '213-240', '320', '400-420', '560-640', ), 'app', 'Hangouts'),
-	('GApps', 'keep', ('nodpi', ), 'app', 'PrebuiltKeep'),
-	('GApps', 'keyboardgoogle', ('nodpi', ), 'app', 'LatinImeGoogle'),
-	('GApps', 'maps', ('213-240', '320', '400-480', '560-640', ), 'app', 'Maps'),
-	('GApps', 'messenger', ('nodpi', '160', '240', '320', '480', '640', ), 'app', 'PrebuiltBugle'),
-	('GApps', 'movies', ('nodpi', ), 'app', 'Videos'),
-	('GApps', 'music', ('nodpi', ), 'app', 'Music2'),
-	('GApps', 'newsstand', ('nodpi', ), 'app', 'Newsstand'),
-	('GApps', 'newswidget', ('nodpi', ), 'app', 'PrebuiltNewsWeather'),
-	('GApps', 'packageinstallergoogle', ('nodpi', ), 'priv-app', 'GooglePackageInstaller'),
-	('GApps', 'photos', ('nodpi', '160', '240', '320', '400-420-480-560', ), 'app', 'Photos'),
-	('GApps', 'playgames', ('nodpi', '160', '240', '320', '480', ), 'app', 'PlayGames'),
-	('GApps', 'search', ('nodpi', '160', '240', '320', ), 'priv-app', 'Velvet'),
-	('GApps', 'sheets', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsSheets'),
-	('GApps', 'slides', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsSlides'),
-	('GApps', 'taggoogle', ('nodpi', ), 'priv-app', 'TagGoogle'),
-	('GApps', 'talkback', ('nodpi', ), 'app', 'talkback'),
-	('GApps', 'translate', ('nodpi', ), 'app', 'TranslatePrebuilt'),
-	('GApps', 'webviewgoogle', ('nodpi', ), 'app', 'WebViewGoogle'),
-	('GApps', 'youtube', ('nodpi', '160', '240', '320', '480', ), 'app', 'YouTube'),
-	('GApps', 'cameragoogle', ('nodpi', ), 'app', 'GoogleCamera'),
-	('GApps', 'dialergoogle', ('nodpi', ), 'priv-app', 'GoogleDialer'),
+	('GApps', 'androidpay', 'arm', ('nodpi', '480', '640',), 'app', 'Wallet'),
+	('GApps', 'books', 'all', ('nodpi', ), 'app', 'Books'),
+	('GApps', 'calculatorgoogle', 'all', ('nodpi', ), 'app', 'CalculatorGoogle'),
+	('GApps', 'calendargoogle', 'all', ('nodpi', ), 'app', 'CalendarGooglePrebuilt'),
+	('GApps', 'calsync', 'all', ('nodpi', ), 'app', 'GoogleCalendarSyncAdapter'),
+	('GApps', 'cameragoogle', 'arm', ('nodpi', ), 'app', 'GoogleCamera'),
+	('GApps', 'chrome', 'arm', ('nodpi', ), 'app', 'Chrome'),
+	('GApps', 'clockgoogle', 'all', ('nodpi', ), 'app', 'PrebuiltDeskClockGoogle'),
+	('GApps', 'cloudprint', 'all', ('nodpi', ), 'app', 'CloudPrint2'),
+	('GApps', 'contactsgoogle', 'all', ('nodpi', ), 'priv-app', 'GoogleContacts'),
+	('GApps', 'dialergoogle', 'all', ('nodpi', ), 'priv-app', 'GoogleDialer'),
+	('GApps', 'dmagent', 'all', ('nodpi', ), 'app', 'DMAgent'),
+	('GApps', 'docs', 'arm', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsDocs'),
+	('GApps', 'drive', 'arm', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'Drive'),
+	('GApps', 'ears', 'all', ('nodpi', ), 'app', 'GoogleEars'),
+	('GApps', 'earth', 'arm', ('nodpi', '120-160', '213-240', '320', '480',), 'app', 'GoogleEarth'),
+	('GApps', 'exchangegoogle', 'all', ('nodpi', ), 'app', 'PrebuiltExchange3Google'),
+	('GApps', 'faceunlock', 'all', ('nodpi', ), 'app', 'FaceLock'),
+	('GApps', 'fitness', 'all', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'FitnessPrebuilt'),
+	('GApps', 'gcs', 'all', ('nodpi', ), 'priv-app', 'GCS'),
+	('GApps', 'gmail', 'all', ('nodpi', ), 'app', 'PrebuiltGmail'),
+	('GApps', 'googlenow', 'all', ('nodpi', ), 'app', 'GoogleHome'),
+	('GApps', 'googleplus', 'arm', ('nodpi', '480',), 'app', 'PlusOne'),
+	('GApps', 'googletts', 'arm', ('nodpi', ), 'app', 'GoogleTTS'),
+	('GApps', 'hangouts', 'arm', ('160', '213-240', '320', '400-420', '560-640', ), 'app', 'Hangouts'),
+	('GApps', 'keep', 'arm', ('nodpi', ), 'app', 'PrebuiltKeep'),
+	('GApps', 'keyboardgoogle', 'arm', ('nodpi', ), 'app', 'LatinImeGoogle'),
+	('GApps', 'maps', 'arm', ('213-240', '320', '400-480', '560-640', ), 'app', 'Maps'),
+	('GApps', 'messenger', 'arm', ('nodpi', '160', '240', '320', '480', '640', ), 'app', 'PrebuiltBugle'),
+	('GApps', 'movies', 'arm', ('nodpi', ), 'app', 'Videos'),
+	('GApps', 'music', 'all', ('nodpi', ), 'app', 'Music2'),
+	('GApps', 'newsstand', 'all', ('nodpi', ), 'app', 'Newsstand'),
+	('GApps', 'newswidget', 'all', ('nodpi', ), 'app', 'PrebuiltNewsWeather'),
+	('GApps', 'packageinstallergoogle', 'all', ('nodpi', ), 'priv-app', 'GooglePackageInstaller'),
+	('GApps', 'photos', 'arm', ('nodpi', '160', '240', '320', '400-420-480-560', ), 'app', 'Photos'),
+	('GApps', 'playgames', 'arm', ('nodpi', '160', '240', '320', '480', ), 'app', 'PlayGames'),
+	('GApps', 'search', 'arm', ('nodpi', '160', '240', '320', ), 'priv-app', 'Velvet'),
+	('GApps', 'sheets', 'arm', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsSheets'),
+	('GApps', 'slides', 'arm', ('nodpi', '160', '240', '320', '480', '640',), 'app', 'EditorsSlides'),
+	('GApps', 'taggoogle', 'all', ('nodpi', ), 'priv-app', 'TagGoogle'),
+	('GApps', 'talkback', 'all', ('nodpi', ), 'app', 'talkback'),
+	('GApps', 'translate', 'arm', ('nodpi', ), 'app', 'TranslatePrebuilt'),
+	('GApps', 'webviewgoogle', 'arm', ('nodpi', ), 'app', 'WebViewGoogle'),
+	('GApps', 'youtube', 'arm', ('nodpi', '160', '240', '320', '480', ), 'app', 'YouTube'),
 )
 OCCAM_APKS = (
-	('Core', 'setupwizarddefault', ('nodpi', ), 'priv-app', 'SetupWizard'),
+	('Core', 'setupwizarddefault', 'all', ('nodpi', ), 'priv-app', 'SetupWizard'),
 )
 NAKASIG_APKS = (
-	('Core', 'setupwizardtablet', ('nodpi', ), 'priv-app', 'SetupWizard'),
+	('Core', 'setupwizardtablet', 'all', ('nodpi', ), 'priv-app', 'SetupWizard'),
 )
 
 # Type the following command in the opengapps build directory: find . -name *.xml
 # Only results of command above starting with ./Core or ./GApps
 # List vary with build variant
 OPENGAPPS_DATA = [
-    './Core/defaultetc/common/etc/sysconfig/google.xml',
-    './Core/defaultetc/common/etc/sysconfig/google_build.xml',
-    './Core/defaultetc/common/etc/sysconfig/whitelist_com.android.omadm.service.xml',
-    './Core/defaultetc/common/etc/preferred-apps/google.xml',
-    './Core/defaultframework/common/etc/permissions/com.google.android.maps.xml',
-    './Core/defaultframework/common/etc/permissions/com.google.android.media.effects.xml',
-    './Core/defaultframework/common/etc/permissions/com.google.widevine.software.drm.xml',
-    './GApps/dialerframework/common/etc/permissions/com.google.android.dialer.support.xml',
-    './GApps/cameragooglelegacy/common/etc/permissions/com.google.android.camera2.xml',
-    './GApps/cameragoogle/common/etc/permissions/com.google.android.camera.experimental2015.xml' ]
+    './Core/defaultetc-all/common/etc/sysconfig/google.xml',
+    './Core/defaultetc-all/common/etc/sysconfig/google_build.xml',
+    './Core/defaultetc-all/common/etc/sysconfig/whitelist_com.android.omadm.service.xml',
+    './Core/defaultetc-all/common/etc/preferred-apps/google.xml',
+    './Core/defaultframework-all/common/etc/permissions/com.google.android.maps.xml',
+    './Core/defaultframework-all/common/etc/permissions/com.google.android.media.effects.xml',
+    './Core/defaultframework-all/common/etc/permissions/com.google.widevine.software.drm.xml',
+    './GApps/dialerframework-all/common/etc/permissions/com.google.android.dialer.support.xml',
+    './GApps/cameragoogle-all/common/etc/permissions/com.google.android.camera.experimental2015.xml' ]
 # Type the following command in the opengapps build directory: find . -name *.bin
 # Only results of command above starting with ./Core or ./GApps
 # List vary with build variant
 OPENGAPPS_FACELOCK_6 = [
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/landmark_group_meta_data.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-3-tree7-wmd.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-r0-ri30.4a-v24-tree7-2-wmd.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-r.8.1.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-y-r.8.1.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rn30-ri30.5-v24-tree7-2-wmd.bin',
-    './GApps/faceunlock/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rp30-ri30.5-v24-tree7-2-wmd.bin' ]
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/landmark_group_meta_data.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32-tree7-wmd.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.8/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-3-tree7-wmd.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-r0-ri30.4a-v24-tree7-2-wmd.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-r.8.1.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/pose-y-r.8.1.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rn30-ri30.5-v24-tree7-2-wmd.bin',
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/detection/yaw_roll_face_detectors.7.1/head-y0-yi45-p0-pi45-rp30-ri30.5-v24-tree7-2-wmd.bin' ]
 OPENGAPPS_FACELOCK_5 = [
-    './GApps/faceunlock/common/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin' ]
+    './GApps/faceunlock-all/common/vendor/pittpatt/models/recognition/face.face.y0-y0-71-N-tree_7-wmd.bin' ]
 # Type the following command in the opengapps build directory: find . -name *.so
 # Only results of command above starting with ./Core or ./GApps
 # List vary with build variant
 OPENGAPPS_FACELOCK_2 = [
-    './GApps/faceunlock/common/vendor/lib/libfrsdk.so',
-    './GApps/faceunlock/common/lib/libfacelock_jni.so',
-    './GApps/facedetect/common/lib/libfilterpack_facedetect.so' ]
+    './GApps/faceunlock-arm/common/vendor/lib/libfrsdk.so',
+    './GApps/faceunlock-arm/common/lib/libfacelock_jni.so',
+    './GApps/facedetect-arm/common/lib/libfilterpack_facedetect.so' ]
 # Type the following command in the opengapps build directory: find . -name *.jar
 # Only results of command above starting with ./Core or ./GApps
 # List vary with build variant
 OPENGAPPS_JAR = [
-    './Core/defaultframework/common/framework/com.google.widevine.software.drm.jar',
-    './Core/defaultframework/common/framework/com.google.android.media.effects.jar',
-    './Core/defaultframework/common/framework/com.google.android.maps.jar',
-    './GApps/dialerframework/common/framework/com.google.android.dialer.support.jar',
-    './GApps/cameragooglelegacy/common/framework/com.google.android.camera2.jar',
-    './GApps/cameragoogle/common/framework/com.google.android.camera.experimental2015.jar' ]
+    './Core/defaultframework-all/common/framework/com.google.widevine.software.drm.jar',
+    './Core/defaultframework-all/common/framework/com.google.android.media.effects.jar',
+    './Core/defaultframework-all/common/framework/com.google.android.maps.jar',
+    './GApps/dialerframework-all/common/framework/com.google.android.dialer.support.jar',
+    './GApps/cameragoogle-all/common/framework/com.google.android.camera.experimental2015.jar' ]
 # Type the following command in the opengapps build directory: find . -name *.so
 # Only results of command above starting with ./Core or ./GApps
 # List vary with build variant
 OPENGAPPS_LIB = [
-    './Optional/swypelibs/common/lib/libjni_latinimegoogle.so', 
-    './Optional/swypelibs/common/lib/libjni_keyboarddecoder.so' ]
+    './Optional/swypelibs-arm/common/lib/libjni_latinimegoogle.so', 
+    './Optional/swypelibs-arm/common/lib/libjni_keyboarddecoder.so' ]
 
 # Required android tools
 AAPT = 'aapt'
@@ -231,10 +228,10 @@ def dpi_selection(device_name, dpis):
 
 def copy_opengapps_apk(apks):
     for apk in apks:
-        location, opengapps_appname, dpis, system_dir, appname = apk
+        location, opengapps_appname, build_arch, dpis, system_dir, appname = apk
         device_dpi = dpi_selection(device_name, dpis)
         if device_dpi != '':
-            src = os.path.join(opengapps_path, location, opengapps_appname, device_dpi, system_dir, appname, appname + '.apk')
+            src = os.path.join(opengapps_path, location, opengapps_appname + '-' + build_arch, device_dpi, system_dir, appname, appname + '.apk')
             dst = os.path.join(vendor_dir, APPS_DIR, appname + '.apk')
             try:
                 print 'Copying %s...' %src
